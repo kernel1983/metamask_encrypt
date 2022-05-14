d791752d41e73b705bc6c1be0c70769481d2bfcd
KJ <kernel1983@gmail.com>
2022-05-14 11:55:48 +0800

    add packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,29 @@
 const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development'
+    mode: 'development',
+    resolve: {
+        fallback: {
+            // url: require.resolve('url'),
+            // crypto: require.resolve('crypto-browserify'),
+            // http: require.resolve('stream-http'),
+            // https: require.resolve('https-browserify'),
+            // os: require.resolve('os-browserify/browser'),
+            // fs: require.resolve('fs'),
+            process: require.resolve('process'),
+            assert: require.resolve('assert'),
+            stream: require.resolve('stream-browserify'),
+            Buffer: require.resolve('buffer'),
+        },
+    },
+    plugins: [
+        // Work around for Buffer is undefined:
+        // https://github.com/webpack/changelog-v5/issues/10
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],
 }
