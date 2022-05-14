b37d250d814e6bee084577861c92e566952a0ea5
KJ <kernel1983@gmail.com>
2022-05-14 12:17:15 +0800

    encrypt and decrypt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { encrypt } from '@metamask/eth-sig-util';
 const ascii85 = require('ascii85');
 
+// Reference:
+// https://betterprogramming.pub/exchanging-encrypted-data-on-blockchain-using-metamask-a2e65a9a896c
 
 function encryptData(publicKey, data) {
   // Returned object contains 4 properties: version, ephemPublicKey, nonce, ciphertext
@@ -29,6 +31,27 @@ function encryptData(publicKey, data) {
   
   // Return just the Buffer to make the function directly compatible with decryptData function
   return buf;
+}
+
+
+async function decryptData(account, data) {
+  // Reconstructing the original object outputed by encryption
+  const structuredData = {
+    version: 'x25519-xsalsa20-poly1305',
+    ephemPublicKey: data.slice(0, 32).toString('base64'),
+    nonce: data.slice(32, 56).toString('base64'),
+    ciphertext: data.slice(56).toString('base64'),
+  };
+  // Convert data to hex string required by MetaMask
+  const ct = `0x${Buffer.from(JSON.stringify(structuredData), 'utf8').toString('hex')}`;
+  // Send request to MetaMask to decrypt the ciphertext
+  // Once again application must have acces to the account
+  const decrypt = await window.ethereum.request({
+    method: 'eth_decrypt',
+    params: [ct, account],
+  });
+  // Decode the base85 to final bytes
+  return ascii85.decode(decrypt);
 }
 
 window.addEventListener('load', async function () {
@@ -63,6 +86,16 @@ window.addEventListener('load', async function () {
         ethAccounts.append(div);
       }
     })
+
+    const public_key = await ethereum.request({ method: 'eth_getEncryptionPublicKey', params: [accounts[0]] });
+    console.log(public_key);
+
+    const data = Buffer.from('master secret', 'ascii');
+    const encrypted = encryptData(public_key, data);
+    console.log(encrypted);
+
+    const decrypted = await decryptData(accounts[0], encrypted);
+    console.log(decrypted.toString('ascii'));
   };
 })
 
